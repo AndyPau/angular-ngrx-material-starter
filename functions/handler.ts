c511b2c5fcd3650dfab98f0e1754cd9f8987637b
andy <andy.pau@qq.com>
2018-07-24 17:16:50 +0800

    add aws functions

--- functions/handler.ts
+++ functions/handler.ts
@@ -5,9 +5,9 @@ export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Cal
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      input: event
+    })
   };
 
   cb(null, response);
-}
+};
